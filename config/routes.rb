1134f362f820c10128dbd5c21d57c789ac9749fb
Aamir Shah <aamir.shah@sourcebits.com>
2013-01-22 14:02:14 +0530

    added micro_post resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 FirstApp::Application.routes.draw do
+  resources :micro_posts
+
+
   resources :users
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130122075409) do
+ActiveRecord::Schema.define(:version => 20130122083126) do
+
+  create_table "micro_posts", :force => true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
